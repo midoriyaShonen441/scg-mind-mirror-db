dfb75dc9960fcf4210eee8eb67c42fc078b497d0
“midoriyaShonen441” <khunnatta@gmail.com>
2022-06-06 13:22:36 +0700

    edit model

--- API/model/writeMood.js
+++ API/model/writeMood.js
@@ -8,8 +8,20 @@ const userMood = new Schema(
         email:{ type:String, default:null }, 
         phone:{ type:String, default:null }, 
         address:{ type:String, default:null }, 
-        voiceMood:{ type:String, default:null }, 
-        faceMood:{ type:String, default:null }
+        voiceMood:{ 
+            word:{ type:String, default:null },
+            select_lang:{ type:String, default:null },
+            sentiment_score:{ type:Number, default:null },
+            sentiment_magnitude: { type:Number, default:null },
+            feeling: { type:String, default:null }
+        }, 
+        faceMood:{ 
+            anger: { type:String, default:null },
+            joy:{ type:String, default:null },
+            surprise:{ type:String, default:null },
+            sorrow:{ type:String, default:null },
+            highest_emotion:{ type:String, default:null }
+         }
     },
     {
         timestamps: true
